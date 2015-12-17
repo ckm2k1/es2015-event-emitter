11eed1fc29a6c60236305c4bad71fba99af2026d
Stanislav Venzerul <stanislav_venzerul@silanis.com>
2015-12-16 22:49:13 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,82 @@
+class EventEmitter {
+  constructor() {
+    this._listeningTo = new Map();
+    this._listeners = new Map();
+  }
+
+  listenTo(obj, evt, listener, ctx) {
+    let sym = this._listeningTo.get(obj) || Symbol();
+    let listenerMap = obj._listeners || (obj._listeners = new Map());
+    let events = listenerMap.get(sym);
+    if (!events) {
+      events = {};
+      listenerMap.set(sym, events);
+    }
+
+    let listenerObj = {
+      fn: listener,
+      ctx: ctx
+    };
+
+    /*jshint expr: true*/
+    (events[evt] && events[evt].listeners.push(listenerObj)) || (events[evt] = {
+      listeners: [listenerObj]
+    });
+
+    this._listeningTo.set(obj, sym);
+
+    return this;
+  }
+
+  stopListening(obj, evt, listener) {
+    if (!obj) {
+      this._listeningTo.forEach((sym, obj) => {
+        if (obj._listeners.has(sym)) obj._listeners.delete(sym);
+      });
+      this._listeningTo.delete(obj);
+
+      return this;
+    }
+
+    if (obj && evt && !listener) {
+      this._listeningTo.forEach((sym, obj) => {
+        let events = obj._listeners.get(sym);
+        if (events && events[evt]) delete events[evt];
+      });
+
+      return this;
+    }
+
+    if (obj && evt && listener) {
+      this._listeningTo.forEach((sym, obj) => {
+        let events = obj._listeners.get(sym);
+        if (events && events[evt]) {
+          let i = events[evt].listeners.findIndex(listener);
+          if (i !== -1) events[evt].listeners.splice(i, 1);
+        }
+      });
+    }
+
+    return this;
+  }
+
+  trigger(evt, ...args) {
+    for (let eventMap of this._listeners.values()) {
+      let ev = eventMap[evt];
+      if (ev) ev.listeners.forEach(listener => {
+        listener.fn.call(listener.ctx || null, ...args);
+      });
+    }
+
+    return this;
+  }
+
+  listenToOnce(obj, evt, listener, ctx) {
+    this.listenTo(obj, evt, (...args) => {
+      listener.call(ctx || null, ...args);
+      this.stopListening(obj, evt, listener);
+    }, ctx);
+  }
+}
+
+module.exports = EventEmitter;
---
+++ tests/index.js
@@ -0,0 +1,67 @@
+/*jshint expr:true*/
+'use strict';
+
+let EventEmitter = require('../');
+let chai = require('chai');
+let expect = chai.expect;
+let sinon = require('sinon');
+let sinonChai = require('sinon-chai');
+chai.use(sinonChai);
+
+class Listener extends EventEmitter {}
+class Publisher extends EventEmitter {}
+
+describe('ES6 EventEmitter Object', function () {
+  let li, pr;
+
+  before(function() {
+    li = new Listener();
+    pr = new Publisher();
+  });
+
+  it('should call the correct listener', function () {
+    let spy = sinon.spy();
+    let spy2 = sinon.spy();
+
+    li.listenTo(pr, 'event1', spy);
+    li.listenTo(pr, 'event2', spy2);
+    pr.trigger('event1');
+
+    expect(spy).to.have.been.calledOnce;
+    pr.trigger('event1');
+    pr.trigger('event1');
+    pr.trigger('event1');
+    pr.trigger('event1');
+    expect(spy).to.have.callCount(5);
+    expect(spy2).to.not.have.been.called;
+  });
+
+  it('should forward arguments to the listener', function () {
+    let spy = sinon.spy();
+    li.listenTo(pr, 'change', spy);
+    pr.trigger('change', 'abc', 123);
+
+    expect(spy).to.have.been.calledOnce;
+    expect(spy).to.have.been.calledWith('abc', 123);
+  });
+
+  it('should listenToOnce only once...', function () {
+
+  });
+
+  it('should stopListening to all events', function () {
+
+  });
+
+  it('should stopListening to one specific object', function () {
+
+  });
+
+  it('should stopListening to one specific event on an object', function () {
+
+  });
+
+  it('should stopListening only on one listener', function () {
+
+  });
+});
